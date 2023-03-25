bad71a5603c5055e93b1f6402d1ccf16999c2c3f
Jonah Hornak <45068818+chargedpika@users.noreply.github.com>
2023-03-25 12:50:44 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023mawne",
+      "defaultValue": "2023mawor",
       "required": "true",
       "disabled": "true"
     },
