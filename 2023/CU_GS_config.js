4e6f625183448f7ab068d16c51ef49d88a9edd28
Jonah Hornak <45068818+chargedpika@users.noreply.github.com>
2023-04-06 14:40:49 -0400

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "NECMP1",
+      "defaultValue": "2023NECMP1",
       "required": "true",
       "disabled": "true"
     },
