ecd4d490685d54ad5488611aff3b96cfef310956
Jonah Hornak <45068818+chargedpika@users.noreply.github.com>
2023-04-05 21:42:47 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023mawor",
+      "defaultValue": "NECMP1",
       "required": "true",
       "disabled": "true"
     },
